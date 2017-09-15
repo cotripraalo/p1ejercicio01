c9fb023ad8d81bac4483c16785295b797c24d48d
alumno <alumno@LIMSFLAB04-14D.system-uap.adm>
2017-09-14 19:06:51 -0500

    firts

---
+++ src/ejercicio_1/Ejercicio_1.java
@@ -0,0 +1,26 @@
+package ejercicio_1;
+
+import java.util.Scanner;
+
+public class Ejercicio_1 {
+
+    public static void main(String[] args) {
+        //Creando Scanner
+        Scanner input = new Scanner(System.in);
+        //Declarando variables
+        int numero1, numero2,resultado=0;
+        
+        //Mostrar mensaje
+        System.out.println("Ingrese el numero a multiplicar: ");
+        numero1=input.nextInt();
+        System.out.println("Ingrese el numero a multiplicar: ");
+        numero2=input.nextInt();
+        
+        for(int n=numero2 ; numero2>0; numero2--){
+            resultado=resultado + numero1;
+        }
+        //Mostrar el resultado final
+        System.out.println("El resultado de la multiplicacion es: " + resultado);
+    }
+    
+}
